4fdb2cff9c3d057133cb28195258eb1fd9ff3408
Andres Camilo <andresdm97@hotmail.com>
2022-07-18 18:07:07 -0500

    quinto commit, tutorial terminado

--- main.js
+++ main.js
@@ -10,24 +10,32 @@
 
     self.Board.prototype = {
         get elements() {
-            let elements = this.bars.map(function(bar){return bar;});
+            let elements = this.bars.map(function (bar) { return bar; });
             elements.push(this.ball);
             return elements;
         }
     }
 })();
 
-(function(){
-    self.Ball = function(x,y,radius, board){
-        this.x =x;
-        this.y =y;
+(function () {
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
         this.radius = radius;
-        this.speed_y =0;
+        this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
         board.ball = this;
         this.kind = "circle";
 
+
+    }
+    self.Ball.prototype = {
+        move: function () {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y * this.direction);
+        }
     }
 })();
 
@@ -77,8 +85,11 @@
             }
         },
         play: function () {
-            this.clean();
-            this.draw();
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -103,21 +114,32 @@ let canvas = document.getElementById('canvas');
 let board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
+board_view.draw();
 window.requestAnimationFrame(controller);
 
 document.addEventListener("keydown", function (ev) {
-    ev.preventDefault();
     if (ev.keyCode == 38) {
+        ev.preventDefault();
         bar.up();
     }
     else if (ev.keyCode == 40) {
+        ev.preventDefault();
+
         bar.down();
     }
     else if (ev.keyCode == 87) {
+        ev.preventDefault();
+
         bar2.up();
     }
     else if (ev.keyCode == 83) {
+        ev.preventDefault();
+
         bar2.down();
+    }
+    else if (ev.keyCode == 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 
 })
