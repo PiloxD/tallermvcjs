ae9e9b60209e027f4b2250dc7bd382d997a06a9b
Andres Camilo <andresdm97@hotmail.com>
2022-07-18 17:04:37 -0500

    tercer commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function () {
-    self.Board = function (width, height) {  
+    self.Board = function (width, height) {
         this.width = width;
         this.height = height;
         this.playing = false;
@@ -26,14 +26,18 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function () {
-
+            this.y += this.speed;
         },
         up: function () {
-
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: "+ this.x + "y: "+ this.y;
         }
     }
 })();
@@ -67,14 +71,23 @@
         }
     }
 })();
+let board = new Board(800, 400);
+var bar = new Bar(20, 100, 100, 20, board);
+var bar = new Bar(760, 100, 100, 20, board);
+let canvas = document.getElementById('canvas');
+let board_view = new BoardView(canvas, board);
+document.addEventListener("keydown", function (ev) {
+    if (ev.keyCode == 38) {
+        bar.up();
+    }
+    else if (ev.keyCode == 40) {
+        bar.down();
+    }
+
+})
 
 self.addEventListener("load", main);
 function main() {
-    let board = new Board(800, 400);
-    var bar = new Bar(20, 100, 100, 20, board);
-    var bar = new Bar(760, 100, 100, 20, board);
 
-    let canvas = document.getElementById('canvas');
-    let board_view = new BoardView(canvas, board);
     board_view.draw();
 }
